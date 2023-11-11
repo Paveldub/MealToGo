806b00a1c8701a0a732e8b7301004297e50be320
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-11-11 22:11:28 +0530

    styled components

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
 import React from "react";
-import { Searchbar } from "react-native-paper";
-import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restourants.screen";
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar placeholder="Search" />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
-      <ExpoStatusBar style="auto" />
+      <RestaurantsScreen />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});

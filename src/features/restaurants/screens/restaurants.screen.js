d88039957c6dd5a5ebf92e239bdb86993e047ead
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-11-17 23:11:23 +0530

    preloader data

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,13 +1,14 @@
 import React, { useContext } from "react";
 import { Searchbar } from "react-native-paper";
 import { FlatList } from "react-native";
-import { RestaurantInfoCard } from "../components/restourant-info-card.component";
 import styled from "styled-components";
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 import { Spacer } from "../../../components/Spacer/spacer.component";
 import { SearchContainer } from "./restaurants.screen.styles";
 import { SafeArea } from "../../../components/utils/safe-area.components";
 import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
+import { RestaurantInfoCard } from "../components/restourant-info-card.component";
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -15,11 +16,26 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
 
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={MD2Colors.blue300} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar placeholder="Search" />
       </SearchContainer>

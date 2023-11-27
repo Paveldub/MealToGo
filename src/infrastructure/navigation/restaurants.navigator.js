a5b3b887b282de0e31244619f2a296ae7a619fc8
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-11-27 22:14:57 +0530

    list item for restaurant card

--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text } from "react-native";
+
 import {
   createStackNavigator,
   TransitionPresets,
 } from "@react-navigation/stack";
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
+import { RestaurantDetailsScreen } from "../../features/restaurants/screens/restaurant-details.screen";
 
 const RestaurantStack = createStackNavigator();
 
@@ -20,7 +21,7 @@ export const RestaurantsNavigator = () => {
       />
       <RestaurantStack.Screen
         name="RestauantsDetail"
-        component={() => <Text>Restauants Detail</Text>}
+        component={RestaurantDetailsScreen}
       />
     </RestaurantStack.Navigator>
   );

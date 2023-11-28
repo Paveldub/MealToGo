7c2179a249130adb5c9ada3ac870c1e8ae48e718
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-11-29 00:18:30 +0530

    restaurant pins with correct data and images

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,16 +1,61 @@
-import React from "react";
-import MapView from "react-native-maps";
+import React, { useContext, useEffect, useState } from "react";
+import MapView, { Callout, Marker } from "react-native-maps";
 import styled from "styled-components";
 import { Search } from "../components/search.component";
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restaurant.context";
+import { MapCallout } from "../components/map-callout.components";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-export const MapScreen = () => (
-  <>
-    <Search />
-    <Map />
-  </>
-);
+export const MapScreen = () => {
+  const { location } = useContext(LocationContext);
+  const { restaurants = [] } = useContext(RestaurantsContext);
+
+  const [latDelta, setLatDelta] = useState(0);
+
+  const { lat, lng, viewport } = location;
+
+  useEffect(() => {
+    const northeastLat = viewport.northeast.lat;
+    const southwestLat = viewport.southwest.lat;
+
+    setLatDelta(northeastLat - southwestLat);
+  }, [location, viewport]);
+
+  return (
+    <>
+      <Search />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView key={restaurant.name}>
+              <Marker
+                key={restaurant.name}
+                coordinate={{
+                  latitude: restaurant.geometry.location.lat,
+                  longitude: restaurant.geometry.location.lng,
+                }}
+                title={restaurant.name}
+              >
+                <Callout>
+                  <MapCallout restaurant={restaurant} />
+                </Callout>
+              </Marker>
+            </MapView>
+          );
+        })}
+      </Map>
+    </>
+  );
+};

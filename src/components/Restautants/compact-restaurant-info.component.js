c550c2cf02c8697a8975150ade023281b8f773e4
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-11-29 22:20:51 +0530

    import { WebView } from 'react-native-webview';

--- src/components/Restautants/compact-restaurant-info.component.js
+++ src/components/Restautants/compact-restaurant-info.component.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text } from "../Typography/Typography.component";
 import { WebView } from "react-native-webview";
 import { Platform } from "react-native";
+
+import { Text } from "../Typography/Typography.component";
 
 const CompactImage = styled.Image`
   border-radius: 10px;
@@ -30,7 +31,7 @@ export const CompactRestaurantInfo = ({ restaurant }) => {
   return (
     <Item>
       <Image source={{ uri: restaurant.photos[0] }} />
-      <Text enter variant="caption" numberOfLines={3}>
+      <Text center variant="caption" numberOfLines={3}>
         {restaurant.name}
       </Text>
     </Item>

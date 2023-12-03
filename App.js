c41fee145cc49a35791c8420c69d2c32eab5e760
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-12-03 22:46:57 +0530

    firebase for app

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { initializeApp } from 'firebase/app';
+
 import { theme } from "./src/infrastructure/theme";
 
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurant.context";
@@ -14,6 +16,18 @@ import { LocationContextProvider } from "./src/services/location/location.contex
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
 
 import { Navigation } from "./src/infrastructure/navigation";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBj-sT7W89om0lGYQLogZaPKKe6vbi5nrE",
+  authDomain: "mealstogo-6dc6b.firebaseapp.com",
+  projectId: "mealstogo-6dc6b",
+  storageBucket: "mealstogo-6dc6b.appspot.com",
+  messagingSenderId: "585266902026",
+  appId: "1:585266902026:web:22284b44fae75c38935bf3",
+  measurementId: "G-5R87XC0DBX",
+};
+
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({

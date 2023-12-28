063d8e7f7165992b96359b7fcf3af6a8b95b8fd4
Pavel Demidovich <pdzemidovich@hubspot.com>
2023-12-28 23:52:48 +0530

    production correct urls

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,9 @@
-const liveHost = "https://us-central1-mealstogo-b2612.cloudfunctions.net";
+const liveHost = "https://us-central1-mealstogo-6dc6b.cloudfunctions.net";
 const localHost = "http://127.0.0.1:5001/mealstogo-6dc6b/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 
 export const host = isDevelopment ? localHost : liveHost;
+
+// !!! android test https !!!
+// export const host = liveHost;
